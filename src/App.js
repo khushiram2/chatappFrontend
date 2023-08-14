8381cf1595d3f1b6c5bca72dbbd7e847271253cd
khushi <khushiramrajliwal@gmail.com>
2023-08-14 13:06:43 +0530

    chatAppbyKhushiRam

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './conponents/Home';
+import { Loginpage } from './conponents/Loginpage';
+import { Registerpage } from './conponents/Registerpage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
+import { Chat } from './conponents/Chat';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <ToastContainer /> 
+    </div>
+      <Routes>
+      <Route  path='/' element={<Navigate replace to="/login"/>}  />
+      <Route  path='/home' Component={Home}  />
+      <Route  path='/login' Component={Loginpage}  />
+      <Route  path='/register' Component={Registerpage}  />
+      <Route  path='/chat' Component={Chat}  />
+      </Routes>
     </div>
   );
 }
---
+++ src/conponents/Registerpage.js
@@ -0,0 +1,130 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Api } from "./GlobalApi";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from 'react-toastify';
+
+
+
+export const Registerpage = () => {
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email("not valid email").required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf(
+        [Yup.ref("password")],
+        "Password and confirm password must be same"
+      )
+      .required("Confirm Password is required"),
+  });
+
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+    onSubmit: (values, { resetForm }) => {
+      axios
+        .post(`${Api}/user/register`, { user: values })
+        .then((res) => {if(res.status===200){
+          window.localStorage.setItem("token",res.data.token)
+            toast.success(res.data.message,{autoClose:5000})
+            navigate("/chat")
+        }else{
+          toast.error(res.data.message,{autoClose:5000})
+          navigate("/login")
+        }})
+        .then(() => resetForm())
+        .catch(() => {
+          toast.error("some error occured")
+          navigate("/login")
+        });
+    },
+    validationSchema: validationSchema,
+  });
+
+
+
+
+
+  return (
+    <div className="form-container">
+      <div className="form" style={{ height: "500px" }}>
+        <div className="heading">
+          <h1>Register</h1>
+        </div>
+        <div className="form-data">
+          <label className="form-lable" htmlFor="name">
+            Enter your name
+          </label>
+          <input
+            onChange={formik.handleChange}
+            type="text"
+            className="form-input"
+            name="name"
+          />
+          {formik.touched.name && formik.errors.name && (
+            <div className="error">{formik.errors.name}</div>
+          )}
+          <label className="form-lable" htmlFor="email">
+            Enter your email
+          </label>
+          <input
+            onChange={formik.handleChange}
+            type="email"
+            className="form-input"
+            name="email"
+          />
+          {formik.touched.email && formik.errors.email && (
+            <div className="error">{formik.errors.email}</div>
+          )}
+          <label className="form-lable" htmlFor="password">
+            Enter your password
+          </label>
+          <input
+            onChange={formik.handleChange}
+            type="password"
+            className="form-input"
+            name="password"
+          />
+          {formik.touched.password && formik.errors.password && (
+            <div className="error">{formik.errors.password}</div>
+          )}
+          <label className="form-lable" htmlFor="confirm-password">
+            confirm password
+          </label>
+          <input
+            onChange={formik.handleChange}
+            type="password"
+            className="form-input"
+            name="confirmPassword"
+          />
+          {formik.touched.confirmPassword && formik.errors.confirmPassword && (
+            <div className="error">{formik.errors.confirmPassword}</div>
+          )}
+          <button type="submit" onClick={formik.handleSubmit} className="btn">
+            submit
+          </button>
+          <div className="sorR" style={{ marginTop: "10px" }}>
+            already have an account? &nbsp;{" "}
+            <p
+              style={{ margin: "0px", cursor: "pointer", color: "blue" }}
+              onClick={() => navigate("/login")}
+            >
+              {" "}
+              sign in{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
